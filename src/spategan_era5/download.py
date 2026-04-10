1e9ae4f8360762d8bc67442d1520a90d67b5c159
JohannesSchnell <j.schnell@52north.org>
2026-04-10 09:30:25 +0200

    added ecmwf api client; noted alternative download source for hist datat (azure cloud)

--- src/spategan_era5/download.py
+++ src/spategan_era5/download.py
@@ -13,7 +13,7 @@ def prepare_ecmwf_data(out_dir: Path, date: str, forecast_steps: list) -> Path:
     clean_filename = out_dir / f"ecmwf_{date}_clean.nc"
 
     logger.info(f"Downloading AIFS data for date '{date}' and steps '{forecast_steps}'")
-    client = Client(source="ecmwf", model="aifs-single")
+    client = Client(source="ecmwf", model="aifs-single") #historic data under source='azure'
     client.retrieve(
             date=date,
             time=0,
@@ -46,15 +46,3 @@ def prepare_ecmwf_data(out_dir: Path, date: str, forecast_steps: list) -> Path:
 
     return clean_filename
 
-#
-# ds = xr.open_dataset("/work/PROJEKTE/VELOCITYADAPT/code/spateGAN/data/aifs/ecmwf_20260330_aifs.grib2")
-# ds = ds.rename({
-#     'valid_time': 'time',
-#     'time': 'start_time',
-# })
-# ds = ds.swap_dims({'step': 'time'})
-# ds['lsp'] = ds['tp'] - ds['cp']
-#
-# ds_hourly = ds.resample(time="1h").interpolate(kind='linear')
-# ds_hourly_rate2 = (ds_hourly.shift(time=-1) - ds_hourly).dropna(dim="time")
-# ds_hourly_rate = ds_hourly.diff(dim="time")
